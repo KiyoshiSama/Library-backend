49fe1e3678ad4fbb84410eaae682e37a92a91f26
amls <amirhoseinbr83@gmail.com>
2024-07-27 16:21:38 +0330

    changed urls to  standard

--- core/core/urls.py
+++ core/core/urls.py
@@ -10,17 +10,17 @@ from drf_spectacular.views import (
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/account/", include("accounts.api.urls")),
-    path("api/book-management/", include("books.api.urls")),
-    path("api/reserve/", include("transactions.api.urls")),
+    path("api/v1/account/", include("accounts.api.urls")),
+    path("api/v1/book-management/", include("books.api.urls")),
+    path("api/v1/reserve/", include("transactions.api.urls")),
     path("api-auth/", include("rest_framework.urls")),
     path("api/schema-download/", SpectacularAPIView.as_view(), name="schema"),
     path(
-        "swagger/",
+        "api/v1/docs/",
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
     ),
-    path("redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    path("api/v1/redocs/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
