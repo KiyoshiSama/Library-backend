e89be8d44c37b9dd15fa8d6b35aa1e0eb31d6f6c
amls <amirhoseinbr83@gmail.com>
2024-07-27 15:31:39 +0330

    changed swagger and redoc urls

--- core/core/urls.py
+++ core/core/urls.py
@@ -16,11 +16,11 @@ urlpatterns = [
     path("api-auth/", include("rest_framework.urls")),
     path("api/schema-download/", SpectacularAPIView.as_view(), name="schema"),
     path(
-        "api/swagger/",
+        "swagger/",
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
     ),
-    path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    path("redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
